7ad9a776e2861fbb3afa52fac7aa8f1586eedb3a
Pedro Canute <pedro.canute2@gmail.com>
2026-05-08 14:45:15 -0300

    Criado o arquivo inicial do projeto

---
+++ Challenge2/Challenge2/Challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge2App.swift
+//  Challenge2
+//
+//  Created by Pedro Canute on 08/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge2/Challenge2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge2
+//
+//  Created by Pedro Canute on 08/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

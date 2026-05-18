24fd28d27eada0f07eb3a42325ec144d556de658
Pedro Canute <pedro.canute2@gmail.com>
2026-05-18 18:54:00 -0300

    Criado componente BotaoPrincipalView e EscolhaUmaPlantaView

---
+++ Challenge2/Challenge2/View/Componentes/BotaoPrincipal/BotaoPrincipalView.swift
@@ -0,0 +1,18 @@
+//
+//  BotaoPrincipalView.swift
+//  Challenge2
+//
+//  Created by Pedro Canute on 18/05/26.
+//
+import SwiftUI
+
+
+struct BotaoPrincipalView: View {
+	var body: some View {
+		Text("Teste Creato")
+			.font(.custom("CreatoDisplay-Thin.otf", size: 45))
+	}
+}
+#Preview {
+	BotaoPrincipalView()
+}

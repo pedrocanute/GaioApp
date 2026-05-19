9fd7b4ef3f20f4c599027091ba9e144e89ec11f7
Guilherme Alves de Souza <guimourast@gmail.com>
2026-05-19 15:37:26 -0300

    Terminei a Tela de Escolha do Ambiente

--- Challenge2/Challenge2/View/EscolherAmbiente/EscolherAmbienteView.swift
+++ Challenge2/Challenge2/View/EscolherAmbiente/EscolherAmbienteView.swift
@@ -10,23 +10,34 @@ import SwiftUI
 struct EscolherAmbienteView: View{
 	var body: some View{
         
-        VStack (alignment: .trailing){
+        VStack {
             ZStack(){
-                Image("Elipse1")
-                Text("Escolha do ambente")
-                    
-                    
+                HStack{
+                    Spacer()
+                    Image("Elipse1")
+                }
+                Text("Escolha do ambiente")
+                    .font(.custom("Lalezar-Regular", size: 30))
+                    .foregroundColor(.corFonteVerdeEscuro)
             }
             
             Text("Em qual ambiente gostaria de ver o seu Girassol? ")
-            
+                .font(.custom("CreatoDisplay-Regular", size: 24))
+                .foregroundColor(.corFonteVerdeEscuro)
+                .multilineTextAlignment(.center)
+                .lineLimit(2)
+                .frame(width: 300)
+                
             BotaoEscolherAmbienteView(botao  :BotaoEscolherAmbienteViewModel.botaoAmbiente[0])
                 .padding(30)
             BotaoEscolherAmbienteView(botao  :BotaoEscolherAmbienteViewModel.botaoAmbiente[1])
                 .padding(30)
+            
+            BotaoPrincipalView()
+                .frame(width: 209)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background()
+        .background(Color.corFundoBege)
 	}
         
 }

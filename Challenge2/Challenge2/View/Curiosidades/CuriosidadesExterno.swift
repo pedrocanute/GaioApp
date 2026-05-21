78361278e451cb2e214a55125e27193cc2bc6507
monge <pedrokamonge@gmail.com>
2026-05-21 17:42:22 -0300

    tela de curiosidade do girasol externo terminada

--- Challenge2/Challenge2/View/Curiosidades/CuriosidadesExterno.swift
+++ Challenge2/Challenge2/View/Curiosidades/CuriosidadesExterno.swift
@@ -4,14 +4,77 @@
 //
 //  Created by Pedro Canute on 18/05/26.
 //
+
 import SwiftUI
 
-struct CuriosidadesExternoView: View {
-	var body: some View{
-		
-	}
+struct CuriosidadesExterno: View {
+    
+    @StateObject private var pesquisaExteno = FAQVCuriosidadeExternoViewModel()
+    
+    @Environment(\.horizontalSizeClass) var tamanhoHorizontal
+    
+    @Environment(\.verticalSizeClass) var tamanhoVertical
+    
+    var body: some View {
+        GeometryReader{ geo in
+            
+            let noIPad = tamanhoHorizontal == .regular
+            
+            let tituloSize = noIPad ? 50 : 35.0
+            
+            let titoloPesquisa = noIPad ? 30.0 : 20.0
+            
+            let pesquisaSize = noIPad ? 25.0 : 17.0
+            
+            let imagemSize = noIPad ? 450.0 : 250.0
+            
+            VStack{
+                Spacer()
+                VStack{
+                    Text(AttributedString("Curiosidades do\nGirassol no Jardim"))
+                        .font(.custom("lalezar-regular", size: tituloSize, relativeTo: .title3))
+                        .multilineTextAlignment(.center)
+                        .lineLimit(nil)
+                        .foregroundStyle(Color.corFonteVerdeEscuro)
+                }
+                VStack{
+                    
+            List(pesquisaExteno.questions) { question in
+                DisclosureGroup {
+                    Image(question.image )
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity, maxHeight: imagemSize, alignment: .center)
+                    
+                    Text(AttributedString(question.pesquisaTex))
+                        .font(.custom("creatoDisplay-regular", size: pesquisaSize, relativeTo: .title2))
+                    
+                    
+                } label: {
+                    Text(question.titleTex)
+                        .font(.custom("creatoDisplay-medium", size: titoloPesquisa, relativeTo: .title2))
+                }
+                .listRowBackground(Color.corElipseFundo)
+                .listRowSeparator(.hidden)
+                
+            }
+           
+            .listStyle(.automatic)
+            .scrollContentBackground(.hidden)
+      
+             }
+                .cornerRadius(40)
+            
+                
+                Spacer()
+            }
+            .background(Color.corFundoBege)
+        }
+    }
 }
 
-#Preview {
-	CuriosidadesExternoView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CuriosidadesExterno()
+    }
 }

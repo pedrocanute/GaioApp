a29bdae033160aca83a87ec67494820ccaccb553
monge <pedrokamonge@gmail.com>
2026-05-27 16:19:47 -0300

    navegaçao do picker terminada

--- Challenge2/Challenge2/View/EscolherPlanta/EscolherPlantaIPhoneView.swift
+++ Challenge2/Challenge2/View/EscolherPlanta/EscolherPlantaIPhoneView.swift
@@ -7,32 +7,32 @@
 import SwiftUI
 
 struct EscolherPlantaIPhoneView: View {
-var body: some View {
-NavigationStack{
-VStack(alignment: .center) {
-
-Spacer()
-
-Image("logoGaio").resizable()
-.frame(maxWidth: 256, maxHeight: 308)
-Spacer()
-
-Text("O quebra-galho do cultivo de plantas")
-.font(.custom("CreatoDisplay-Regular", size: 20))
-.multilineTextAlignment(.center)
-.foregroundStyle(.black)
-
-Spacer()
-
-PainelEscolhaView(destino: EscolherAmbienteIphoneView())
-.frame(height: 300)
-
-}
-.background(Color.corFundoBege.ignoresSafeArea())
-}
-}
+    var body: some View {
+        NavigationStack{
+            VStack(alignment: .center) {
+                
+                Spacer()
+                
+                Image("logoGaio").resizable()
+                    .frame(maxWidth: 256, maxHeight: 308)
+                Spacer()
+                
+                Text("O quebra-galho do cultivo de plantas")
+                    .font(.custom("CreatoDisplay-Regular", size: 20))
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.black)
+                
+                Spacer()
+                
+                PainelEscolhaView(destino: EscolherAmbienteIphoneView())
+                    .frame(height: 300)
+                
+            }
+            .background(Color.corFundoBege.ignoresSafeArea())
+        }
+    }
 }
 
 #Preview {
-EscolherPlantaIPhoneView()
+    EscolherPlantaIPhoneView()
 }
